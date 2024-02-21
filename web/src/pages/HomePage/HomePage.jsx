e4090866c4655cd8e2c35b3637f58a558d3a207a
encayni <oganisyanararat1@gmail.com>
2024-02-21 17:09:46 +0400

    hero section & sticky nav

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,20 @@
+/* eslint-disable no-unused-vars */
+import background from 'public/assets/background.png'
+
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+
+import styles from './homePage.module.css'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
-
-      <h1>HomePage</h1>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <img className={styles.background} src={background} alt="background" />
+      <div style={{ marginTop: 250 }}></div>
+      <h1>Салон красоты</h1>
+      <h1>«Delote-Beauty»</h1>
+      <h1>на Крестовском</h1>
     </>
   )
 }
